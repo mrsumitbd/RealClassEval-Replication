1a0904dc3c109c6759d26a378a3669a84e40ebeb
Musfiqur (Sumit) Rahman <mrsumitbd@users.noreply.github.com>
2025-10-30 23:58:02 -0400

    Update rag_main.py

--- src/rag/rag_main.py
+++ src/rag/rag_main.py
@@ -5,7 +5,7 @@ import sys
 df = pd.read_csv(sys.argv[1])
 if sys.argv[1].split("/dfs")[0].split("_data/")[1] == 'csn':
     convert_dataframe_to_dataset(
-        df.sample(n = 5000, random_state=42).reset_index(drop=True),
+        df.sample(n = 5000, random_state=42).reset_index(drop=True), # using a sample of 5000 from csv (pre-cutoff) to avoid memory overflow
         output_path=f'../../rag_experiments/{sys.argv[1].split("/dfs")[0].split("_data/")[1]}.json',
         include_metadata=False  # or True if you want metrics
     )
